202ee1aaea488ecc51e151a4ea0e48721c880c4f
vernyuy <fonchu.e.venyuy@gmail.com>
2024-01-28 14:09:02 +0100

    Added unit test

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -12,7 +12,7 @@ interface LambdaStackProps extends StackProps {
 export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
-
+    
     const table: dynamodb.Table = new dynamodb.Table(
       this,
       "CdkTypescriptWeatherTable",
@@ -52,103 +52,103 @@ export class LambdaStack extends Stack {
         },
       }
     );
-    // // Grant createWeatherLambda the permission to write to dynamodb table
+    // Grant createWeatherLambda the permission to write to dynamodb table
     table.grantReadWriteData(createWeatherLambda);
 
-    // // Defining a lambda resource to read data from dynamodb table
-    // const getWeatherLambda: lambda.Function = new lambda.Function(
-    //   this,
-    //   "getWeatherLambdaFunction",
-    //   {
-    //     functionName: "cdk-typescript-get",
-    //     runtime: lambda.Runtime.NODEJS_14_X,
-    //     handler: "getWeather.lambdaHandler",
-    //     code: lambda.Code.fromAsset("src"),
-    //     environment: {
-    //       TABLE_NAME: table.tableName,
-    //       STAGE_NAME: props.stageName,
-    //     },
-    //   }
-    // );
+    // Defining a lambda resource to read data from dynamodb table
+    const getWeatherLambda: lambda.Function = new lambda.Function(
+      this,
+      "getWeatherLambdaFunction",
+      {
+        functionName: "cdk-typescript-get",
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: "getWeather.lambdaHandler",
+        code: lambda.Code.fromAsset("src"),
+        environment: {
+          TABLE_NAME: table.tableName,
+          STAGE_NAME: props.stageName,
+        },
+      }
+    );
 
-    // // Grant getWeatherLambda the permission to read data from dynamodb table
-    // table.grantReadData(getWeatherLambda);
+    // Grant getWeatherLambda the permission to read data from dynamodb table
+    table.grantReadData(getWeatherLambda);
 
-    // // Defining a lambda resource to list all data from dynamodb table
-    // const listWeatherLambda: lambda.Function = new lambda.Function(
-    //   this,
-    //   "listWeatherLambdaFunction",
-    //   {
-    //     functionName: "cdk-typescript-list",
-    //     runtime: lambda.Runtime.NODEJS_14_X,
-    //     handler: "listWeathers.lambdaHandler",
-    //     code: lambda.Code.fromAsset("src"),
-    //     environment: {
-    //       TABLE_NAME: table.tableName,
-    //       STAGE_NAME: props.stageName,
-    //     },
-    //   }
-    // );
+    // Defining a lambda resource to list all data from dynamodb table
+    const listWeatherLambda: lambda.Function = new lambda.Function(
+      this,
+      "listWeatherLambdaFunction",
+      {
+        functionName: "cdk-typescript-list",
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: "listWeathers.lambdaHandler",
+        code: lambda.Code.fromAsset("src"),
+        environment: {
+          TABLE_NAME: table.tableName,
+          STAGE_NAME: props.stageName,
+        },
+      }
+    );
 
-    // // Grant listWeatherLambda the permission to read data from dynamodb table
-    // table.grantReadData(listWeatherLambda);
+    // Grant listWeatherLambda the permission to read data from dynamodb table
+    table.grantReadData(listWeatherLambda);
 
-    // // Defining a lambda resource to delete data from dynamodb table
-    // const deleteWeatherLambda: lambda.Function = new lambda.Function(
-    //   this,
-    //   "deleteWeatherLambdaFunction",
-    //   {
-    //     functionName: "cdk-typescript-delete",
-    //     runtime: lambda.Runtime.NODEJS_14_X,
-    //     handler: "deleteWeather.lambdaHandler",
-    //     code: lambda.Code.fromAsset("src"),
-    //     environment: {
-    //       TABLE_NAME: table.tableName,
-    //       STAGE_NAME: props.stageName,
-    //     },
-    //   }
-    // );
+    // Defining a lambda resource to delete data from dynamodb table
+    const deleteWeatherLambda: lambda.Function = new lambda.Function(
+      this,
+      "deleteWeatherLambdaFunction",
+      {
+        functionName: "cdk-typescript-delete",
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: "deleteWeather.lambdaHandler",
+        code: lambda.Code.fromAsset("src"),
+        environment: {
+          TABLE_NAME: table.tableName,
+          STAGE_NAME: props.stageName,
+        },
+      }
+    );
 
-    // // Grant deleteWeatherLambda the permission to delete data from dynamodb table
-    // table.grantReadWriteData(deleteWeatherLambda);
+    // Grant deleteWeatherLambda the permission to delete data from dynamodb table
+    table.grantReadWriteData(deleteWeatherLambda);
 
-    // // Defining a lambda resource to update data in dynamodb table
-    // const updateWeatherLambda: lambda.Function = new lambda.Function(
-    //   this,
-    //   "updateWeatherLambdaFunction",
-    //   {
-    //     functionName: "cdk-typescript-update",
-    //     runtime: lambda.Runtime.NODEJS_14_X,
-    //     handler: "updateWeather.lambdaHandler",
-    //     code: lambda.Code.fromAsset("src"),
-    //     environment: {
-    //       TABLE_NAME: table.tableName,
-    //       STAGE_NAME: props.stageName,
-    //     },
-    //   }
-    // );
+    // Defining a lambda resource to update data in dynamodb table
+    const updateWeatherLambda: lambda.Function = new lambda.Function(
+      this,
+      "updateWeatherLambdaFunction",
+      {
+        functionName: "cdk-typescript-update",
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: "updateWeather.lambdaHandler",
+        code: lambda.Code.fromAsset("src"),
+        environment: {
+          TABLE_NAME: table.tableName,
+          STAGE_NAME: props.stageName,
+        },
+      }
+    );
 
-    // // Grant updateWeatherLambda the permission to update data in dynamodb table
-    // table.grantReadWriteData(updateWeatherLambda);
+    // Grant updateWeatherLambda the permission to update data in dynamodb table
+    table.grantReadWriteData(updateWeatherLambda);
 
-    // // Defining an api route to create a weather item
+    // Defining an api route to create a weather item
     weatherApi.root
       .addResource("create-weather")
       .addMethod("POST", new apigw.LambdaIntegration(createWeatherLambda));
 
-    // // Defining an api route to read a weather item
-    // const weathers: apigw.Resource = weatherApi.root.addResource("weather");
-    // const weather: apigw.Resource = weathers.addResource("{id}");
-    // // Defining an api route to list all weather items
-    // weathers.addMethod("GET", new apigw.LambdaIntegration(listWeatherLambda));
-    // weather.addMethod("GET", new apigw.LambdaIntegration(getWeatherLambda));
-    // // Defining an api route to delete a weather items
-    // weather.addMethod(
-    //   "DELETE",
-    //   new apigw.LambdaIntegration(deleteWeatherLambda)
-    // );
+    // Defining an api route to read a weather item
+    const weathers: apigw.Resource = weatherApi.root.addResource("weather");
+    const weather: apigw.Resource = weathers.addResource("{id}");
+    // Defining an api route to list all weather items
+    weathers.addMethod("GET", new apigw.LambdaIntegration(listWeatherLambda));
+    weather.addMethod("GET", new apigw.LambdaIntegration(getWeatherLambda));
+    // Defining an api route to delete a weather items
+    weather.addMethod(
+      "DELETE",
+      new apigw.LambdaIntegration(deleteWeatherLambda)
+    );
 
-    // // Defining an api route to update a weather item
-    // weather.addMethod("PUT", new apigw.LambdaIntegration(updateWeatherLambda));
+    // Defining an api route to update a weather item
+    weather.addMethod("PUT", new apigw.LambdaIntegration(updateWeatherLambda));
   }
 }
